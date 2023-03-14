46be5a070d1b98c1877a9fb3c37cd1b8739d9b02
yulia tsygankova <yulia.tsygankova@constantcontact.com>
2023-03-14 17:25:29 -0400

    Write test scenarios for New Contact Form

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# updater
-test assignment for the interview
+# Updater
+Test assignment for the interview.
+
+How to run the tests:
+1. Open a terminal or command prompt and navigate to your project directory.
+2. Open the Cypress Test Runner: 'npx cypress open'. This will launch the Cypress Test Runner, you'll need to click on a test file to run its tests.
+   2.a. To run all the tests in the project headlessly: 'npx cypress run'. 
+   2.b. To run a specific test file: 'npx cypress run --spec "cypress/integration/my-test.spec.js"'
---
+++ cypress/e2e/contactForm-spec.js
@@ -0,0 +1,84 @@
+const testData = require('../fixtures/contactForm.json')
+// in a real project all locators also would be in a separate file
+
+describe('Creating New Contact Form', () => {
+    beforeEach(() => {
+        cy.visit('/new-contact'); // this will extent the base url from cypress.json
+    });
+
+    it('should display the correct title', () => {
+        cy.get('h1').should('contain', 'Creating New Contact');
+    });
+
+    it('should display the correct fields', () => {
+        cy.get('label').should('contain', 'Contact Type');
+        cy.get('label').should('contain', 'Customer Number');
+        cy.get('label').should('contain', 'First Name');
+        cy.get('label').should('contain', 'Office Phone');
+        cy.get('label').should('contain', 'Home Phone');
+        cy.get('label').should('contain', 'Primary Email');
+        cy.get('label').should('contain', 'Last Name');
+        cy.get('label').should('contain', 'Mobile Phone');
+        cy.get('label').should('contain', 'Department');
+        cy.get('label').should('contain', 'Secondary Email');
+    });
+
+    it('should require Contact Type in the Custom Information section', () => {
+        cy.get('[data-cy=contact-type]').select(' '); // or another way to deselect the dropdown
+        cy.get('[data-cy=save]').click();
+        cy.get('.error').should('contain', 'Please select a Contact Type'); // all banners text is an assumption
+    });
+
+    it('should require Last Name in the Basic Information section', () => {
+        cy.get('[data-cy=contact-type]').select(testData.contactType);
+        cy.get('[data-cy=save]').click();
+        cy.get('.error').should('contain', 'Please enter a Last Name');
+    });
+
+    it('should save with mandatory fields only', () => {
+        cy.get('[data-cy=contact-type]').select(testData.contactType);  // Transferee is hardcoded
+        cy.get('[data-cy=last-name]').type(testData.lastName);
+        cy.get('[data-cy=save]').click();
+        // it is unclear what should happen after saving. If a new conformation page should be open, here should be an assert for it
+
+        // if the same form should be present, there should be a banner and the form should be cleared
+        // or all info should be saved. The example below is for all info saved scenario
+        cy.get('.error').should('not.exist');
+        cy.get('.success').should('contain', 'Contact saved successfully');
+        cy.url().should('contain', '/contact-details');
+        cy.contains(testData.contactType);
+        cy.contains(testData.customerNumber);
+    });
+
+    it('should allow optional fields to be filled in', () => {
+        cy.get('[data-cy=contact-type]').select(testData.contactType);
+        cy.get('[data-cy=customer-number]').type(testData.customerNumber);
+        cy.get('[data-cy=last-name]').type(testData.lastName);
+        cy.get('[data-cy=first-name]').type(testData.firstName);
+        cy.get('[data-cy=office-phone]').type(testData.officePhone);
+        cy.get('[data-cy=home-phone]').type(testData.homePhone);
+        cy.get('[data-cy=primary-email]').type(testData.primaryEmail);
+        cy.get('[data-cy=mobile-phone]').type(testData.mobilePhone);
+        cy.get('[data-cy=department]').type(testData.department);
+        cy.get('[data-cy=secondary-email]').type(testData.secondaryEmail);
+        cy.get('[data-cy=save]').click();
+        cy.get('.error').should('not.exist');
+        cy.get('.success').should('contain', 'Contact saved successfully');
+    });
+
+    it('should be able to cancel the form', () => {
+        cy.get('[data-cy=cancel]').click();
+        cy.url().should('not.contain', '/new-contact'); // here should be an assertion for the previous or home page
+    });
+})
+
+// Due to time limit I won't implement the details, but show the idea
+describe('Negative scenarios for New Contact Form', () => {
+    it('the form does not submit if invalid phone is entered', () => {});
+    it('the form does not submit if invalid email is entered', () => {});
+    it('the form correctly handles special characters in input fields', () => {});
+    it('the form correctly handles long input strings or has max characters limit', () => {});
+    it('the form does not allow duplicate entries', () => {});
+    it('the form correctly handles multiple optional fields with the same name', () => {});
+})
+
